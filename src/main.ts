920cc1796ae15671f0537b91f505eb050c81fb4e
yangswseven <992601497@qq.com>
2023-06-03 14:01:53 +0800

    test husky

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
+
+// const a = 100
+// console.log(a)
+
+const fun = () => 
+{
+  console.log('这是没有格式化的代码')
+}
+fun()
+
+for(var i = 0; i<100;i++)    {
+  console.log(i);
+  
+}
+
+
